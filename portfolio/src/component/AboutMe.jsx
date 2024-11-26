9ba011d83feb16b8e05f5d3b6cf2dfdb240007c0
Melike <melikedemirci23@gmail.com>
2024-11-26 14:49:25 +0300

    Responsive
    Responsive adjustments made to About Me section

--- portfolio/src/component/AboutMe.jsx
+++ portfolio/src/component/AboutMe.jsx
@@ -3,7 +3,7 @@ import img from "../assets/img.jpg"; // Resmin doğru importu
 
 function AboutMe() {
   return (
-    <div id="about" className="flex flex-col-reverse lg:flex-row lg:mt-0 md:mt-44 sm:mt-28 items-center justify-center h-screen">
+    <div id="about" className="flex flex-col-reverse lg:flex-row lg:mt-0 md:mt-20 sm:mt-8 items-center justify-center h-screen">
       {/* İçerik bölümü */}
       <div className="flex flex-col w-full max-w-3xl gap-6 lg:gap-9 p-8 md:p-12 lg:p-16 lg:mt-8 md:mt-0 items-center lg:items-start ">
         <h1 className="font-yeseva text-center lg:text-left text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
@@ -31,7 +31,7 @@ function AboutMe() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum labore cumque aliquid expedita
           pariatur iste deleniti provident optio repellendus velit.
         </p>
-        <div className="flex items-center justify-center lg:justify-start gap-4">
+        <div className="flex  items-center justify-center lg:justify-start gap-1 md:gap-4">
           <a href="http://www.linkedin.com/in/melike-demirci-790324202" target="_blank" rel="noopener noreferrer">
             <img
               width="60"
@@ -57,14 +57,14 @@ function AboutMe() {
               src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/39/external-discord-chat-for-social-gaming-between-peers-logo-bold-tal-revivo.png" 
               alt="Discord" />
           </a>
-          <button className="font-playfair text-sm font-bold tracking-wider text-white bg-stone-400 hover:bg-stone-500 px-4 py-2 rounded-md md:w-36 md:h-10 ml-2">
+          <button className="font-playfair text-sm font-bold tracking-wider text-white bg-stone-400 hover:bg-stone-500 px-4 py-2 rounded-md w-36 h-10 sm:w-36 sm:h-10 md:w-36 md:h-10 ml-2">
             RESUME CV
           </button>
         </div>
       </div>
 
       {/* Resim bölümü */}
-      <div className="flex items-center justify-center w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 flex-shrink-0 ">
+      <div className="flex items-center justify-center w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 lg:mr-6 flex-shrink-0 ">
         <img className="rounded-full w-full h-full animate-smoothBounce" src={img} alt="About Me" />
       </div>
     </div>
